c141423bf20479f6e7c8e143a78112a631d7f520
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2022-01-04 17:05:46 +0100

    positive constant coeff in log terms

--- src/integrate.jl
+++ src/integrate.jl
@@ -20,19 +20,30 @@ function rationalize(f::PolyElem{qqbar})
     end
 end
 
+function positive_constant_coefficient(f::PolyElem)
+    if constant_coefficient(f)<0
+        return -f
+    else
+        return f
+    end
+end
+    
+
 function Eval(t::SumOfLogTerms; real_output::Bool=true)
     var = string(symbols(parent(t.S))[1])
     F = base_ring(t.R)
     as = roots(t.R)
     if length(as)==degree(t.R)    
-        return [LogTerm(a, polynomial(F, [c(a) for c in coefficients(t.S)], var)) for a in as]
+        return [LogTerm(a, positive_constant_coefficient(
+            polynomial(F, [c(a) for c in coefficients(t.S)], var))) for a in as]
     end
     
     as = roots(t.R, QQBar)  
     us = real.(as)
     vs = imag.(as)
     if iszero(vs) || !real_output
-        return [LogTerm(a, polynomial(QQBar, [c(a) for c in coefficients(t.S)], var)) for a in as]
+        return [LogTerm(a, positive_constant_coefficient(
+            polynomial(QQBar, [c(a) for c in coefficients(t.S)], var))) for a in as]
     end
     r = LogToReal(t)
     result = Term[]
@@ -40,12 +51,12 @@ function Eval(t::SumOfLogTerms; real_output::Bool=true)
         a = as[i]
         u, v = us[i], vs[i]
         if iszero(v)
-            push!(result, LogTerm(rationalize(u), rationalize(
-                polynomial(QQBar, [c(u) for c in coefficients(r.S)], var))))
+            push!(result, LogTerm(rationalize(u), rationalize(positive_constant_coefficient(
+            polynomial(QQBar, [c(u) for c in coefficients(r.S)], var)))))
         elseif v>0
             if !iszero(u)
-                push!(result, LogTerm(rationalize(r.LT.coeff*u), rationalize(
-                polynomial(QQBar, [numerator(c)(u, v)//denominator(c)(u, v) for c in coefficients(r.LT.arg)], var))))
+                push!(result, LogTerm(rationalize(r.LT.coeff*u), rationalize(positive_constant_coefficient(
+                polynomial(QQBar, [numerator(c)(u, v)//denominator(c)(u, v) for c in coefficients(r.LT.arg)], var)))))
             end
             for AT in r.ATs
                 push!(result, AtanTerm(rationalize(AT.coeff*v), rationalize(

80428d087a88333b6c1a4356ae503c8ca332a1d2
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2022-01-14 18:12:31 +0100

    added parametric_problems.jl

--- src/SymbolicIntegration.jl
+++ src/SymbolicIntegration.jl
@@ -4,8 +4,9 @@ include("general.jl")
 include("result_types.jl")
 include("rational_functions.jl")
 include("derivations.jl")
-include("risch_diffeq.jl")
 include("transcendental_functions.jl")
+include("risch_diffeq.jl")
+include("parametric_problems.jl")
 
 include("integrate.jl")
 
